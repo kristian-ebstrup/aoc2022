33a08bd2a4727b78dc372f99b62f95fe55f518e2
Kristian Ebstrup Jacobsen <k.ebstrup@gmail.com>
2022-12-16 14:08:28 +0100

    Day 10: Solved Part 2

--- src/day10.rs
+++ src/day10.rs
@@ -42,7 +42,7 @@ fn parse(input: impl BufRead) -> io::Result<Vec<Vec<String>>> {
 
 fn part_1(commands: &Vec<Vec<String>>) -> Option<i32> {
     let mut cycle: i32 = 0;
-    let mut x_register: i32 = 1;
+    let mut x: i32 = 1;
     let mut sum: i32 = 0;
     let mut counter: i32 = 0;
 
@@ -51,7 +51,7 @@ fn part_1(commands: &Vec<Vec<String>>) -> Option<i32> {
             1 => {
                 cycle += 1;
                 if cycle - (20 + counter * 40) == 0 {
-                    sum += cycle * x_register;
+                    sum += cycle * x;
                     counter += 1;
                 }
             }
@@ -59,12 +59,12 @@ fn part_1(commands: &Vec<Vec<String>>) -> Option<i32> {
                 for _ in 0..2 {
                     cycle += 1;
                     if cycle - (20 + counter * 40) == 0 {
-                        sum += cycle * x_register;
+                        sum += cycle * x;
                         counter += 1;
                     }
                 }
 
-                x_register += command[1].parse::<i32>().unwrap();
+                x += command[1].parse::<i32>().unwrap();
             }
             _ => panic!("Invalid length!"),
         }
@@ -73,6 +73,66 @@ fn part_1(commands: &Vec<Vec<String>>) -> Option<i32> {
     Some(sum)
 }
 
-fn part_2(commands: &Vec<Vec<String>>) -> Option<u32> {
-    unimplemented!()
+struct Screen {
+    width: usize,
+    pixels: Vec<bool>,
+    cycle: i32,
+    xpos: i32,
+}
+
+impl Screen {
+    fn new(width: usize, height: usize) -> Self {
+        Screen {
+            width,
+            pixels: vec![false; width * height],
+            cycle: 0,
+            xpos: 1,
+        }
+    }
+
+    fn switch(&mut self) {
+        self.pixels[(self.cycle - 1) as usize] =
+            (0..2).contains(&(self.xpos - (self.cycle - 1) % 40).abs());
+    }
+
+    fn draw(&self) -> String {
+        let mut s = String::from("\n");
+        for (i, pixel) in self.pixels.iter().enumerate() {
+            if i % self.width == 0 {
+                s.push_str("\n");
+            }
+
+            if *pixel {
+                s.push('#');
+            } else {
+                s.push('.');
+            }
+        }
+
+        s
+    }
+}
+
+fn part_2(commands: &Vec<Vec<String>>) -> Option<String> {
+    let mut crt: Screen = Screen::new(40, 6);
+
+    for command in commands.iter() {
+        match command.len() {
+            1 => {
+                crt.cycle += 1;
+                crt.switch();
+            }
+            2 => {
+                crt.cycle += 1;
+                crt.switch();
+                crt.cycle += 1;
+                crt.switch();
+
+                crt.xpos += command[1].parse::<i32>().unwrap();
+            }
+            _ => panic!(),
+        }
+    }
+
+    Some(crt.draw())
 }

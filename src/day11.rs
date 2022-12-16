d4d2a5688ae720dece9d0b035beed572af48d2fb
Kristian Ebstrup Jacobsen <k.ebstrup@gmail.com>
2022-12-16 15:23:14 +0100

    Day 11: Initial Monkey and Monkeys structs constructed, with the intent of adding methods to those structs to allow the shenanigans to occur

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::io;
 use std::io::prelude::*;
 
@@ -30,14 +31,108 @@ pub fn solve(input: impl BufRead, part: u8) -> io::Result<()> {
 fn parse(input: impl BufRead) -> io::Result<Vec<Vec<String>>> {
     Ok(input
         .lines()
-        .map(|x| x.unwrap().split("\n").map(|s| s.to_string()).collect())
+        .map(|x| {
+            x.unwrap()
+                .split_whitespace()
+                .skip(2)
+                .map(|s| s.split(',').next().unwrap().to_string())
+                .collect()
+        })
+        .filter(|v: &Vec<String>| !v.is_empty())
         .collect())
 }
 
-fn part_1(initial_state: &Vec<Vec<String>>) -> Option<i32> {
-    for state in initial_state.iter() {
-        println!("{:?}", state);
+#[derive(Debug)]
+struct Monkey {
+    items: Vec<u32>,
+    operation: (String, String),
+    test: (u32, u32, u32),
+}
+
+impl Monkey {
+    fn new(
+        state: (
+            &Vec<String>,
+            &Vec<String>,
+            &Vec<String>,
+            &Vec<String>,
+            &Vec<String>,
+        ),
+    ) -> Self {
+        /* The state input is a bit cryptic due to the way the parsing is handled. Effectively,
+         * each monkey is through the parsing divided into their respective information, with all
+         * lines skipping the first two words as they hold no information for all lines. From here,
+         * which word is of interest depends on the line, and thus we end up with a seemingly
+         * incomprehensible monkey state input. However, it should be as follows:
+         *
+         * state.0: A vector of numbers in String format corresponding to the items.
+         * state.1: A vector of length 4, where the third and fourth element corresponds to the
+         *          operation and operation input.
+         * state.2: A vector of length 2, where the last element is the test value (divisible by X)
+         * state.3: A vector of length 4, where the last element is the target monkey if test is
+         *          true
+         * state.4: A vector of length 4, where the last element is the target monkey if test is
+         *          false
+         *
+         * Everything else is unimportant. */
+
+        // read items
+        let items = state.0.iter().map(|s| s.parse::<u32>().unwrap()).collect();
+
+        // read operation
+        let operation = (state.1[2].clone(), state.1[3].clone());
+
+        // read test
+        let test = (
+            state.2[1].parse::<u32>().unwrap(),
+            state.3[3].parse::<u32>().unwrap(),
+            state.4[3].parse::<u32>().unwrap(),
+        );
+
+        Monkey {
+            items,
+            operation,
+            test,
+        }
     }
+}
+
+#[derive(Debug)]
+struct Monkeys {
+    monkeys: Vec<Monkey>,
+}
+
+impl Monkeys {
+    fn new() -> Self {
+        Monkeys {
+            monkeys: Vec::new(),
+        }
+    }
+
+    fn add(&mut self, monkey: Monkey) {
+        self.monkeys.push(monkey);
+    }
+}
+
+fn part_1(input: &Vec<Vec<String>>) -> Option<i32> {
+    let mut monkeys: Monkeys = Monkeys::new();
+
+    for (i, state) in input
+        .iter()
+        .tuples::<(
+            &Vec<String>,
+            &Vec<String>,
+            &Vec<String>,
+            &Vec<String>,
+            &Vec<String>,
+        )>()
+        .enumerate()
+    {
+        monkeys.add(Monkey::new(state));
+    }
+
+    println!("{:?}", monkeys);
+
     Some(0)
 }
 
